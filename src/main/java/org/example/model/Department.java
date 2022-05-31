6456b987043548195482abb9604105199eaa2ad3
befreee <ostretsovalexander@gmail.com>
2022-05-31 17:55:06 +0300

    first commit

---
+++ src/main/java/org/example/model/Department.java
@@ -0,0 +1,62 @@
+package org.example.model;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(namespace = "http://test.org")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Department {
+
+    private String deptNo;
+    private String deptName;
+    private String location;
+
+    @XmlElementWrapper(name = "employees")
+    @XmlElement(name = "employee")
+    private List<Employee> employees;
+
+    /**
+     * This default constructor is required if there are other constructors.
+     */
+    public Department() {
+
+    }
+
+    public Department(String deptNo, String deptName, String location) {
+        this.deptNo = deptNo;
+        this.deptName = deptName;
+    }
+
+    public String getDeptNo() {
+        return deptNo;
+    }
+
+    public void setDeptNo(String deptNo) {
+        this.deptNo = deptNo;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+}
